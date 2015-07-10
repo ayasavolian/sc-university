a8722142aba6822d53a020c86d9fa0313d6ee44f
Arrash <myasavolian@gmail.com>
2015-07-10 11:00:49 -0700

    updated the virtualenv

--- scu.py
+++ scu.py
@@ -5,60 +5,57 @@ from sqlalchemy import *
 from sqlalchemy.sql import *
 
 app = Flask(__name__)
-
-@app.route('/')
-def main():
-    return render_template('home.html')
-
-@app.route("/test")
-def test():
-  return "hello world!"
-
-if __name__ == '__main__':
-    app.run()
-
-##This is related to SQL, get to this after you figure out posting and getting using Python
-
-
-engine = create_engine('sqlite:///tutorial.db')
-
-metadata = MetaData(engine)
-
+'''
 users = Table('users', metadata,
    Column('id', Integer, Sequence('user_id_seq'), primary_key=True),
    Column('name', String(50)),
    Column('fullname', String(50)),
    Column('password', String(12))
 )
+'''
+@app.route('/')
+def main():
+    return render_template('home.html')
 
- i = users.insert()
- i.execute({'name': 'Peter', 'password': 'Kim'},
-           {'name': 'Gary', 'password': 'Lochhead'},
- connec          {'name': 'Laura', 'password': 'Fanning'})
+@app.route("/test")
+def test():
+    connection = engine.connect()
+    i = users.insert()
+    i.execute({'name': 'Peter', 'password': 'Kim'},
+             {'name': 'Gary', 'password': 'Lochhead'},
+              {'name': 'Laura', 'password': 'Fanning'})
 
-connection = engine.connect()
-s = select([users.c.name, users.c.password]).where(users.c.name == 'Laura')
-result = connection.execute(s)
-for row in result:
-    print "name:", row['name']
-    print "password:", row['password']
-connection.close()
+    #connection = engine.connect()
+    return "hello world!"
+
+    '''
+    s = select([users.c.name, users.c.password]).where(users.c.name == 'Laura')
+    result = connection.execute(s)
+    for row in result:
+      return "name:", row['name']
+    connection.close()
+    '''
 
 
 
+    '''
+    i.execute({'name': 'John', 'password': 'johnpassword'},
+            {'name': 'Susan', 'password': 'susanpassword'},
+            {'name': 'Carl', 'password': 'carlpassword'})
 
+    s = users.select()
+    rs = s.execute()
 
-i.execute({'name': 'John', 'password': 'johnpassword'},
-          {'name': 'Susan', 'password': 'susanpassword'},
-          {'name': 'Carl', 'password': 'carlpassword'})
+    row = rs.fetchone()
+    print 'Id:', row[0]
+    print 'Name:', row['name']
+    print 'Password:', row[users.c.password]
 
-s = users.select()
-rs = s.execute()
+    for row in rs:
+      print row.name, 'is', row.password, 'is their password'
+    '''
 
-row = rs.fetchone()
-print 'Id:', row[0]
-print 'Name:', row['name']
-print 'Password:', row[users.c.password]
+if __name__ == '__main__':
+    app.run()
 
-for row in rs:
-    print row.name, 'is', row.password, 'is their password'
+##This is related to SQL, get to this after you figure out posting and getting using Python
