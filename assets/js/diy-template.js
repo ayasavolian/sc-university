79f1a9e27ae4cd23d5f8e86808d97e068613cb07
rsnewton <rnewton@marketo.com>
2015-08-28 12:56:11 -0700

    changes

--- assets/js/diy-template.js
+++ assets/js/diy-template.js
@@ -1,3 +1,54 @@
+// dropdown for the 'My Account' button at the top of the page
+$(document).ready(function(){
+  $('#my-account').mouseenter(function(){
+    var submenu = $('#my-account-options');
+    console.log(submenu);
+
+    submenu.css({
+      position: "absolute",
+      top: $(this).offset().top + $(this).height() + 'px',
+      left: $(this).offset().left + 'px',
+      zIndex: 1000
+    });
+
+    submenu.stop().slideDown(300);
+
+    submenu.mouseleave(function(){
+      $(this).slideup(300);
+    });
+  });
+});
+
+// Hamburger menu on the top left, using the Marketo Ball
+
+
+$(document).ready(function(){
+  $('#marketo-ball').click(function(){
+
+    $('.content-container').css('min-height', $(window).height());
+
+    $('nav').css("opacity", 1);
+
+    var contentWidth = $('.container').width();
+
+    $('.content-container').css("width", contentWidth);
+
+    $('#content-layer').css("display", "block");
+
+    $("#container").animate({"marginLeft": ["50%", 'easeOutExpo']}, {
+            duration: 700
+        });
+  });
+
+$('#content-layer').click(function(){
+
+  });
+
+});
+
+
+
+
 // this is how we do the FAQ question/answer dropdown
 $(document).ready(function(){
 
